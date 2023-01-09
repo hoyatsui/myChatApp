d18de13614f83ea0542b240c8a8217dd20258370
hoyatsui <hoya.tsui@gmail.com>
2023-01-08 22:32:13 -0500

    update Conversation's Header Footer and Background

--- src/components/Cat.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Avatar } from "@mui/material";
-const Cat = () => {
-  return (
-    <Avatar src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" />
-  );
-};
-
-export default Cat;
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,14 +1,30 @@
 import React, { Suspense, lazy } from "react";
 import { Stack, Box } from "@mui/material";
-import { theme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import Chats from "./Chats";
+
+import Conversation from "../../components/Conversation";
+
 // Dynamic import
-// The component Cat is going to take some amount of time to load this img.
+// The compimport Conversation from './../../components/Conversation/index';
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
       <Chats />
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
     </Stack>
   );
 };
